2f6d254a51c16fab908b56bab75ae8ed9480732d
Nigmetov Abdurahman <ab_nigmetov@kbtu.kz>
2026-01-21 22:04:44 +0500

    Add casting and strings chapters

---
+++ w1/strings.py
@@ -0,0 +1,27 @@
+print("Hello")
+print('Hello')
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+b = "Hello, World!"
+print(b[2:5])
+
+a = "Hello, World!"
+print(a.upper())
+print(a.lower())
+print(a.strip())
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
